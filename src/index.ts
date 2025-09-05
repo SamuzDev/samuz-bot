3a78eb735bb9dec836a1631cb32910b38a876ece
Samuz <deyverabanto@gmail.com>
2025-09-05 18:36:47 -0500

    feat: Adding commands for samuzbot

--- src/index.ts
+++ src/index.ts
@@ -620,10 +620,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN as string);
 (async () => {
   try {
     await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID as string,
-        process.env.GUILD_ID as string
-      ),
+      Routes.applicationCommands(process.env.CLIENT_ID as string),
       { body: commands }
     );
     console.log("Slash commands registered!");
